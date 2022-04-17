adbcb75375c4d45064c01707c766060cd58542c6
邹易 <185964885@qq.com>
2022-04-17 17:15:11 +0800

    装箱拆箱研究

---
+++ src/main/java/com/zyyu/ucp/test/TestInteger.java
@@ -0,0 +1,15 @@
+package com.zyyu.ucp.test;
+
+public class TestInteger {
+
+    public static void main(String[] args) {
+        Integer aa = 90;
+//        int bb = 90;
+//        Integer cc =128;
+//        System.out.println(aa == bb);
+//        System.out.println(cc == bb);
+        System.out.println(aa.equals(90));
+//        System.out.println(cc.equals(128));
+//        System.out.println(90 == aa);
+    }
+}
